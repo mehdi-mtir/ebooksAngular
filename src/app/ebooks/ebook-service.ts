8f19fb33a719a6716882d6c5fa59f0f4d8c3b65e
mehdi-mtir <mehdi.mtir@gmail.com>
2025-10-17 12:21:40 +0100

    first commit

---
+++ src/app/ebooks/ebook-service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Ebook } from './ebook';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EbookService {
+  private ebooks : Ebook[] = [];
+
+  //getEbooks
+
+  //addEbook
+  
+}
